1ab0c54f99d1f01d84686f67ed03067e3214eb1e
Sam Lane <theminordeveloper@outlook.com>
2023-10-21 16:55:40 +0100

    Data emitted successfully from server

---
+++ zactrack-parser.py
@@ -0,0 +1,68 @@
+# Configuration files
+
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+#from pyzabbix import ZabbixMetric, ZabbixSender
+
+import configparser
+
+import sys
+from time import sleep
+import json
+import os
+
+import socketio
+
+logger = logging.getLogger('claypaky-parse')
+configPath = filename=os.path.join(sys.path[0], "config.ini")
+
+
+zabbix_ip="192.168.0.116"
+zabbix_packet = []
+
+log_file_handler = TimedRotatingFileHandler(filename=os.path.join(sys.path[0], "runtime.log"), when='D', interval=1, backupCount=10,
+                                    encoding='utf-8',
+                                    delay=False)
+
+log_console_handler = logging.StreamHandler(sys.stdout)
+
+log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+log_file_handler.setFormatter(log_formatter)
+log_console_handler.setFormatter(log_formatter)
+
+
+logger.setLevel(logging.DEBUG)
+
+logger.addHandler(log_file_handler)
+logger.addHandler(log_console_handler)
+
+logger.info("Entering program")
+
+# Logging initialised
+
+#zabbixServer = ZabbixSender(zabbix_server=zabbix_ip, timeout=1)
+
+
+sio = socketio.Client()
+
+sio.connect("ws://192.168.3.24:8082/socket.io/")
+
+sio.emit("zactrack-room-control-join", ("room-system-states", False))
+sio.emit("zactrack-room-control-join", ("room-version-license-information", None))
+sio.emit("zactrack-room-control-join", ("room-client-live-info", None))
+sio.emit("zactrack-room-control-join", ("room-active-show-history-entry", None))
+sio.emit("zactrack-room-control-join", ("room-anchor-live-infos", None))
+sio.emit("zactrack-room-control-join", ("room-all-anchor-tracking-infos", None))
+sio.emit("zactrack-room-control-join", ("room-actor-live-infos", None))
+sio.emit("zactrack-room-control-join", ("room-all-sensor-tracking-information", False))
+sio.emit("zactrack-room-control-join", ("room-fixture-live-runtime-values", None))
+
+@sio.on("actor-live-infos")
+def on_actor_message(data):
+    logger.info(data)
+
+while True:
+    continue
+    #logger.info(sio.receive())
